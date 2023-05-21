c988eda4451bc8da60325d2f9f385a31d4da6d04
Sergei Korobov <sergei.cstl@gmail.com>
2023-05-21 12:06:10 +0300

    create createidea.html v1.0

--- lfg/main/views.py
+++ lfg/main/views.py
@@ -2,8 +2,12 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 
-def index(request, ):
+def allideas(request, ):
     return render(request, 'main/allideas.html', {'title': 'Главная страница'})
+
+
+def createidea(request, ):
+    return render(request, 'main/createidea.html', {'title': 'Главная страница'})
 
 
 def pageNotFound(request, exception):
